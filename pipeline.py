6341ece343ea37a41cb035edae866d597d153a45
Joshua Melnick <jmelnick101@gmail.com>
2025-03-20 15:39:04 -0500

    Added beginning of the pipeline

---
+++ pipeline.py
@@ -0,0 +1,8 @@
+import os
+
+if not os.path.isdir("Genomes"):
+    os.system("mkdir Genomes")
+
+origseqs = ["GCF_014961145.1_ASM1496114v1_genomic.fna","GCF_028532485.1_ASM2853248v1_genomic.fna","GCF_021391435.1_ASM2139143v1_genomic.fna","GCF_004379335.1_ASM437933v1_genomic.fna"]
+
+if not os.path.isfile()
