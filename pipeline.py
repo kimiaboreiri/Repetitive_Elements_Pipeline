8be402cec9cd1e2df05ebd3540e4bb75d16cf3d4
Joshua Melnick <jmelnick101@gmail.com>
2025-03-20 16:03:25 -0500

    Added sections to work on, placed incomplete placeholder commands for ART and SPAdes

--- pipeline.py
+++ pipeline.py
@@ -5,4 +5,18 @@ if not os.path.isdir("Genomes"):
 
 origseqs = ["GCF_014961145.1_ASM1496114v1_genomic.fna","GCF_028532485.1_ASM2853248v1_genomic.fna","GCF_021391435.1_ASM2139143v1_genomic.fna","GCF_004379335.1_ASM437933v1_genomic.fna"]
 
-if not os.path.isfile()
+#add code to download sequences
+
+
+#add code to insert repetitive elements
+
+
+#add code to run art
+
+#art flags: -i = input, -l = read length, -f = fold coverage, -o = output prefix, -m = mean fragment length, -s = standard deviation
+os.system("art_illumina -i {0} -l 151 -f {1} -o {2}".format(genome,coverage,out))
+
+#add code to run spades
+os.system("spades.py -k 77 --careful -1 {0}_1.fastq -2 {0}_2.fastq -o {1}".format(name, outdir))
+
+#add code to run unicycler
