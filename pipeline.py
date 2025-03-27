be1d949ee3a1d835bb23121695bbc5049bd46fde
Joshua Melnick <jmelnick101@gmail.com>
2025-03-27 10:43:25 -0500

    Many updates

--- pipeline.py
+++ pipeline.py
@@ -3,12 +3,10 @@ import os
 if not os.path.isdir("Genomes"): #make a directory to store genomes if it doesn't already exist
     os.system("mkdir Genomes")
 
-origseqs = ["GCF_014961145.1_ASM1496114v1_genomic.fna","GCF_028532485.1_ASM2853248v1_genomic.fna","GCF_021391435.1_ASM2139143v1_genomic.fna","GCF_004379335.1_ASM437933v1_genomic.fna"] #list of what the genome files should download as
-origseqcount = 0 #keeps track of which sequence file when checking downloads
 accession = ["GCF_014961145.1","GCF_028532485.1","GCF_021391435.1","GCF_004379335.1"] #genome accession codes
 
 for x in accession: #loop through accessions
-    if origseqs[origseqcount] not in os.listdir("Genomes"): #the order of the files listed above is the same as the order of the accessions, so check if the file has already been downloaded before calling the accession code
+    if x not in ",".join(os.listdir("Genomes")): #no need to redownload
         os.system(f"datasets download genome accession {x} --include genome") #download genome from refseq
         os.system("unzip ncbi_dataset.zip") #unzip
         os.system("rm ncbi_dataset.zip") #delete zip file which has generic name to prevent overwrite issues
@@ -17,7 +15,6 @@ for x in accession: #loop through accessions
         os.system("rm README.md") #delete extra file to avoid asking for overwrite
         os.system("rm md5sum.txt") #delete extra file to avoid asking for overwrite
         os.system("rmdir ncbi_dataset") #remove empty folder
-    origseqcount += 1 #keep track of the file you are on
 
     
 
@@ -43,7 +40,7 @@ for file in os.listdir("Genomes"):
     "GCF_021391435.1_ASM2139143v1_genomic.fna",
     "GCF_004379335.1_ASM437933v1_genomic.fna"
 ]'''
-
+'''
 #Read the sequence
 #loop over each genome file in the genomes list
 for genomefile in genomes:
@@ -59,7 +56,7 @@ for genomefile in genomes:
 for length in motif_lengths:
     motif = "ATGC" * (length // 4 )         #for making a motif with 100 bp length we need to multiple 4 bases 25 times
 
-
+'''
 simgenomes = []
 
 #add code to run art
@@ -71,13 +68,71 @@ for gen in genomes:
     #for depth in range(10,110,10): #increase depth from 10 to 100 in increments of 10
     for depth in [10,100]: #runs art with depth of 10 and 100
         out = str(gen.split("_")[0]) + "_" + str(gen.split("_")[1]) + "_" + str(depth)
-        #art flags: -i = input, -l = read length, -f = fold coverage, -o = output prefix, -m = mean fragment length, -s = standard deviation
+        #art flags: -p = paired ends, -na = don't output alignment file, -i = input, -l = read length, -f = fold coverage, -o = output prefix, -m = mean fragment length, -s = standard deviation
         if not os.path.isfile("{}.fq".format(out)):
-            os.system("art_illumina -i ../Genomes/{0} -l 151 -f {1} -o {2}".format(gen,depth,out))
+            os.system("art_illumina -p -na -i ../Genomes/{0} -l 151 -m 200 -s 10 -f {1} -o {2}".format(gen,depth,out))
 
-#add code to run spades
-os.chdir("..") #exit the artgens directory
-for name in os.listdir("artgens"): #assemble every simulated genome in the artgens directory
-    os.system("spades.py -k 77 --careful -1 {0}_1.fastq -2 {0}_2.fastq -o {0}_assembly/".format(name))
+'''import os
+import os
+import subprocess
+##directory to input files
+input_dir = "/home/project3/artgens"
 
-#add code to run unicycler
+#making output file
+output = "/home/project3/spades_output"
+os.makedirs(output, exist_ok=True)
+
+#raed1 files in input directory 
+fq_files_read1 = []
+for f in os.listdir(input_dir):
+    if f.endswith('1.fq'):
+        fq_files_read1.append(f)
+
+#run SPAdes for each file
+for fq1 in fq_files_read1:
+    fq2 = fq1.replace('1.fq', '2.fq')
+
+    fq1_path = os.path.join(input_dir, fq1)
+    fq2_path = os.path.join(input_dir, fq2)
+    
+    #name for output
+    base = fq1.replace('1.fq', '')
+    outdir = os.path.join(output, base)
+    os.makedirs(outdir, exist_ok=True)
+    
+
+    #run SPAdes
+    subprocess.run(["spades.py", "-1", fq1_path, "-2", fq2_path, "-o", outdir])
+    print(f"finish:{fq1} and {fq2} output in {outdir}")
+
+
+
+'''
+import os
+import subprocess
+
+input_dir = "/home/project3/artgens"
+output_dir = "/home/project3/unicycler_output"
+os.makedirs(output_dir, exist_ok=True)
+
+#forward read path
+
+fq1_files =[]
+for f in os.listdir(input_dir):
+    if f.endswith('1.fq'):
+        fq1_files.append(f)
+
+for fq1 in fq1_files:
+    fq2 = fq1.replace('1.fq', '2.fq')
+
+    
+    fq1_files_path = os.path.join(input_dir, fq1)
+    fq2_files_path = os.path.join(input_dir, fq2)
+
+    base1 = fq1.replace('1.fq', '')
+    outdir1 = os.path.join(output_dir, base1)
+    os.makedirs(outdir1, exist_ok=True)
+
+##running unicycler
+    subprocess.run(["unicycler", "-1", fq1_files_path, "-2", fq2_files_path, "-o", outdir1])
+    print(f"unicycler finished")
